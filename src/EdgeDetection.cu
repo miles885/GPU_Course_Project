28c76a847cecae8e2d968fa261bfe85919cac2e4
Miles Gapcynski <miles885@gmail.com>
2018-11-14 23:12:31 -0500

    Added CPU implementation of Sobel edge detection algorithm.

--- src/EdgeDetection.cu
+++ src/EdgeDetection.cu
@@ -8,6 +8,7 @@
 
 #include "helper_cuda.h"
 #include "ImageUtils.h"
+#include "Sobel.h"
 
 /**
  * Parses the command-line arguments
@@ -36,13 +37,14 @@ int32_t parseCmdArgs(int32_t argc, char ** argv, std::string & fileName)
 }
 
 /**
- * Apply a sobel filter to grayscale values
+ * Apply a Sobel filter to grayscale values
  *
  * @param format       The file format
  * @param imageWidth   The width of the image to write
  * @param imageHeight  The height of the image to write
  * @param bitsPerPixel The bits per pixel of the image to write
  * @param pixelData    The channel separated RGB pixel data
+ * @param useCPU       Flag denoting whether to use CPU or GPU
  *
  * @return Flag denoting success or failure
  */
@@ -50,15 +52,18 @@ int32_t applySobelFilterGrayscale(const FREE_IMAGE_FORMAT & format,
                                   uint32_t imageWidth, 
                                   uint32_t imageHeight, 
                                   uint32_t bitsPerPixel, 
-                                  const BYTE * pixelData)
+                                  const BYTE * pixelData, 
+                                  bool useCPU)
 {
     // Convert pixel data to grayscale
     uint32_t imageSize = imageWidth * imageHeight;
-    BYTE * grayPixelData = new BYTE[imageSize];
+
+    BYTE * h_grayPixelData = new BYTE[imageSize];
+    BYTE * h_outputPixelData = new BYTE[imageSize];
 
     if(bitsPerPixel == 8)
     {
-        memcpy(grayPixelData, pixelData, imageWidth * imageHeight * sizeof(BYTE));
+        memcpy(h_grayPixelData, pixelData, imageWidth * imageHeight * sizeof(BYTE));
     }
     else
     {
@@ -70,17 +75,28 @@ int32_t applySobelFilterGrayscale(const FREE_IMAGE_FORMAT & format,
                 BYTE g = pixelData[(y * imageWidth) + imageSize + x];
                 BYTE b = pixelData[(y * imageWidth) + (imageSize * 2) + x];
 
-                grayPixelData[(y * imageWidth) + x] = (r + g + b) / 3;
+                h_grayPixelData[(y * imageWidth) + x] = (r + g + b) / 3;
             }
         }
     }
 
-    // Apply sobel filter
+    // Apply Sobel filter using CPU
+    if(useCPU)
+    {
+        applySobelFilterCPU(imageWidth, imageHeight, h_grayPixelData, h_outputPixelData);
+    }
+    // Apply Sobel filter using GPU
+    else
+    {
+        //TODO: Setup device memory
+        //TODO: Execute kernel
+        //TODO: Cleanup device memory
+    }
 
     // Output results
     std::string outputFileName = "grayscale_output.png";
 
-    int32_t status = saveImage(outputFileName, format, imageWidth, imageHeight, 8, grayPixelData);
+    int32_t status = saveImage(outputFileName, format, imageWidth, imageHeight, 8, h_outputPixelData);
 
     if(status == EXIT_FAILURE)
     {
@@ -88,7 +104,8 @@ int32_t applySobelFilterGrayscale(const FREE_IMAGE_FORMAT & format,
     }
 
     // Cleanup
-    delete [] grayPixelData;
+    delete [] h_grayPixelData;
+    delete [] h_outputPixelData;
 
     return EXIT_SUCCESS;
 }
@@ -174,18 +191,24 @@ int32_t main(int32_t argc, char ** argv)
     }
 
     /*
-     * Apply sobel filter to grayscale values
+     * Apply Sobel filter to grayscale values
      */
-    status = applySobelFilterGrayscale(format, imageWidth, imageHeight, bitsPerPixel, pixelData);
+    // Apply Sobel filter using CPU
+    status = applySobelFilterGrayscale(format, imageWidth, imageHeight, bitsPerPixel, pixelData, true);
 
     if(status == EXIT_FAILURE)
     {
         return EXIT_FAILURE;
     }
 
+    // Apply Sobel filter using GPU
+
     /*
-     * Apply sobel filter to HSV channels
+     * Apply Sobel filter to HSV channels
      */
+    // Apply Sobel filter using CPU
+
+    // Apply Sobel filter using GPU
 
     /*
      * Cleanup
--- src/Sobel.h
+++ src/Sobel.h
@@ -6,13 +6,15 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-const int32_t GRAD_X[3][3] = {{1, 0, -1}, {2, 0, -2}, {1, 0, -1}};
-const int32_t GRAD_Y[3][3] = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+#include "ImageUtils.h"
+
+const int32_t GRAD_X[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+const int32_t GRAD_Y[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
 __host__
-void applySobelFilterCPU();
+void applySobelFilterCPU(uint32_t imageWidth, uint32_t imageHeight, const BYTE * pixelData, BYTE * outputPixelData);
 
 __global__
-void applySobelFilterGPU();
+void applySobelFilterGPU(uint32_t imageWidth, uint32_t imageHeight, const BYTE * pixelData, BYTE * outputPixelData);
 
 #endif
